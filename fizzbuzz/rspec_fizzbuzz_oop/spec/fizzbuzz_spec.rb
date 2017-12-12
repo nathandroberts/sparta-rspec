1d537a8b8d4289c17605a813db164c1dc74a3bef
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-12-12 12:21:17 +0000

    test driven development used to create fizzbuzz

--- fizzbuzz/rspec_fizzbuzz_oop/spec/fizzbuzz_spec.rb
+++ fizzbuzz/rspec_fizzbuzz_oop/spec/fizzbuzz_spec.rb
@@ -1,7 +1,35 @@
 require 'fizzbuzz'
 
 describe Fizzbuzz do
-
-
+  before(:each) do
+    @fb = Fizzbuzz.new
+  end
+  it "numbers should both be divisible by 3 and 5 and return true" do
+    expect(@fb.divisible_by(15, 3) && @fb.divisible_by(15, 5)).to be(true)
+  end
+  it "num should be divisible by 3 and return true" do
+    expect(@fb.divisible_by(6, 3)).to be(true)
+  end
+  it "num should be divisible by 5 and return true" do
+    expect(@fb.divisible_by(10, 5)).to be(true)
+  end
+  it "numbers should not be divisible by 3 or 5 and return true" do
+    expect(@fb.divisible_by(15, 3) && @fb.divisible_by(15, 5)).to be(true)
+  end
+  it "first array value should be 1" do
+    @fb.fizzbuzz(1,15)
+    expect(@fb.fizzbuzz_array[0]).to eq("1")
+  end
+  it "Third array value should be 3 fizz" do
+    @fb.fizzbuzz(1,15)
+    expect(@fb.fizzbuzz_array[2]).to eq("3 fizz")
+  end
+  it "Fifth array value should be 5 buzz" do
+    @fb.fizzbuzz(1,15)
+    expect(@fb.fizzbuzz_array[4]).to eq("5 buzz")
+  end
+  it "Fifthteenth array value should be 15 fizzbuzz" do
+    @fb.fizzbuzz(1,15)
+    expect(@fb.fizzbuzz_array[14]).to eq("15 fizzbuzz")
+  end
 end
-
